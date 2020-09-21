6fca0ef5910767d9452d57e1c09936172ba86942
HACKER <trinathswain958@gmail.com>
2020-09-21 13:56:56 +0530

    CyclicDI-CI&SI

---
+++ 011-IocProj11-CyclicDI-ConstructorAndSetterInjection/src/main/java/com/sstech/spring/core/beans/A.java
@@ -0,0 +1,28 @@
+package com.sstech.spring.core.beans;
+
+public class A {
+	private B b;
+	
+	/**
+	 * 
+	 */
+	public A() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * @param b the b to set
+	 */
+	public void setB(B b) {
+		this.b = b;
+	}
+
+	@Override
+	public String toString() {
+		return "A [b=" ;
+	}
+	
+	
+	
+
+}
---
+++ 011-IocProj11-CyclicDI-ConstructorAndSetterInjection/src/main/java/com/sstech/spring/core/test/CyclicDITest.java
@@ -0,0 +1,27 @@
+package com.sstech.spring.core.test;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+import com.sstech.spring.core.beans.A;
+import com.sstech.spring.core.beans.B;
+
+public class CyclicDITest {
+
+	public static void main(String[] args) {
+		 BeanFactory factory=null;
+		 A  a=null;B b=null;
+		//create  IOC container 
+		 factory=new XmlBeanFactory(new ClassPathResource("com/sstech/spring/core/cfgs/applicationContext.xml"));
+		 //get Bean
+		 a=factory.getBean("a1",A.class);
+		 System.out.println(a);
+		 
+		 System.out.println(".........");
+		 b=factory.getBean("b1",B.class);
+		 System.out.println(b);
+
+	}
+
+}
